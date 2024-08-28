f9ccf69f9fbbd064e08c630b1a602235dbee8159
Sujon Ahmed <sujonahmed424@gmail.com>
2024-08-28 10:42:12 +0600

    Update command01.php

--- command01.php
+++ command01.php
@@ -2,6 +2,7 @@
 # sudo apt-get update
 # sudo apt-get install nginx
 # sudo apt install software-properties-common apt-transport-https -y
+# sudo add-apt-repository ppa:ondrej/php && sudo apt-get update && sudo apt -y install php8.2 && sudo apt-get install -y php8.2-cli php8.2-json php8.2-fpm php8.2-mysql php8.2-zip php8.2-gd php8.2-mbstring php8.2-curl php8.2-xml php8.2-bcmath
 # sudo add-apt-repository ppa:ondrej/php && sudo apt-get update && sudo apt -y install php8.1 && sudo apt-get install -y php8.1-cli php8.1-json php8.1-common php8.1-mysql php8.1-zip php8.1-gd php8.1-mbstring php8.1-curl php8.1-xml php8.1-bcmath
 # systemctl status nginx
 
